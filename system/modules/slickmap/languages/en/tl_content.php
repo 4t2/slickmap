2172e7c7a61be073279202c1217fe1caa463b26b
Mario Müller <mario@lingo4u.com>
2011-12-20 13:15:47 +0100

    Englische Lokalisierung hinzugefügt

---
+++ system/modules/slickmap/languages/en/tl_content.php
@@ -0,0 +1,35 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_content']['slickmap_primary_legend'] = 'SlickMap root page';
+$GLOBALS['TL_LANG']['tl_content']['slickmap_utility_legend'] = 'SlickMap utility pages';
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_content']['slickmap_utility_pages'] = array('Utility pages', 'Utility pages like Imprint or Contact.');
+$GLOBALS['TL_LANG']['tl_content']['slickmap_root'] = array('Root page', 'Root page of the sitemap.');
+$GLOBALS['TL_LANG']['tl_content']['slickmap_stop_level'] = array('Stoplevel', 'Depth of the sitemap. Enter 0 to show all levels.');
+$GLOBALS['TL_LANG']['tl_content']['slickmap_articles'] = array('Show articles', 'Show articles if there are more than one article per page.');
+
+/**
+ * References
+ */
+$GLOBALS['TL_LANG']['tl_content']['slickmap_columns']['title'] = array('Columns', 'Columns of the sitemap');
+$GLOBALS['TL_LANG']['tl_content']['slickmap_columns']['reference'] = array(
+	'auto'	=> 'auto',
+	'1'		=> '1 column',
+	'2'		=> '2 columns',
+	'3'		=> '3 columns',
+	'4'		=> '4 columns',
+	'5'		=> '5 columns',
+	'6'		=> '6 columns',
+	'7'		=> '7 columns',
+	'8'		=> '8 columns',
+	'9'		=> '9 columns',
+	'10'	=> '10 columns'
+);
+
+?>
